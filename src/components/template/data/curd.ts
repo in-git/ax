20b59b212ff8e6086baa6101f9348bef00566c3b
in-git <444891953@qq.com>
2024-03-29 10:32:07 +0800

    no message

--- src/components/template/data/curd.ts
+++ src/components/template/data/curd.ts
@@ -1,9 +1,22 @@
 import { __showForm } from './form';
+import { __keys, __table } from './table';
 
-export const __list = async () => {};
+export const __list = async () => {
+  __table.value.loading = true;
+  /* GET LIST */
+  __table.value.loading = false;
+};
 
 export const __edit = async (id: number) => {
   __showForm.value = true;
 };
-export const __delete = async (id: number) => {};
+export const __delete = async (id?: number) => {
+  let ids = id ? [id] : __keys.value;
+  /* Delete ids */
+};
 export const __update = async (id: number) => {};
+export const __menus = [
+  {
+    label: '',
+  },
+];
--- src/components/template/table/table.vue
+++ src/components/template/table/table.vue
@@ -6,7 +6,13 @@
       :table="__table"
       :query="__query"
       v-model:selected-keys="__keys"
-    ></SystemTable>
+    >
+      <template v-slot="{ value }">
+        <template v-if="value.column.dataIndex === 'operation'">
+          <Operation @edit="__edit" :items="__operationList" />
+        </template>
+      </template>
+    </SystemTable>
   </div>
 </template>
 
@@ -14,8 +20,10 @@
 import SystemTable from '@/views/components/table/SystemTable.vue';
 import TableHeadVue from './table-head/head.vue';
 
+import Operation from '@/views/components/table/Operation.vue';
 import { __columns } from '../data/column';
-import { __keys, __query, __table } from '../data/table';
+import { __edit } from '../data/curd';
+import { __keys, __operationList, __query, __table } from '../data/table';
 </script>
 
 <style lang="scss" scoped></style>
