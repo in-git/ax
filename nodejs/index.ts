f8f70a813703eadc77f21dea9973edf760ae2adb
in-git <444891953@qq.com>
2024-04-07 18:40:18 +0800

    添加nodejs 模块

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,9 +1,10 @@
-import cors from 'cors';
-import express, { Response } from 'express';
-import { Server } from 'socket.io';
-import { auth } from './src/auth/verify';
-import { connectWs } from './src/config/ws';
-import publicRouter from './src/modules/public/controller/PublicController';
+import cors from "cors";
+import express, { Response } from "express";
+import { Server } from "socket.io";
+import { auth } from "./src/auth/verify";
+import { connectWs } from "./src/config/ws";
+import publicRouter from "./src/modules/public/controller/PublicController";
+import systemRouter from "./src/modules/system/controller/system.controller";
 
 const app = express();
 const expressServer = app.listen(8888);
@@ -11,26 +12,27 @@ const expressServer = app.listen(8888);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  allowedHeaders: ['Authorization', 'Content-Type'],
-  charset: 'utf-8',
+  allowedHeaders: ["Authorization", "Content-Type"],
+  charset: "utf-8",
 };
 
 app.use(cors(corsOptions));
 app.use(auth);
 
-app.use('/public', publicRouter);
+app.use("/public", publicRouter);
+app.use("/system", systemRouter);
 
 app.use((err, req, res: Response, next) => {
   res.status(500).send({
     code: 500,
-    message: 'Internal Server Error',
+    message: "Internal Server Error",
     data: null,
   });
 });
 
 const io = new Server(expressServer, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
 connectWs(io);
