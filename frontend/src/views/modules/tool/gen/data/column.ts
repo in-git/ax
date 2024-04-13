cdad6630f2ed4778c230d21813eca0274e2ad839
in-git <444891953@qq.com>
2024-04-13 16:57:30 +0800

    no message

--- frontend/src/views/modules/tool/gen/data/column.ts
+++ frontend/src/views/modules/tool/gen/data/column.ts
@@ -1,16 +1,11 @@
 import type { ColumnProps } from '@/types/system';
 
 const codeColumns = ref<ColumnProps[]>([
-  { title: '创建者', dataIndex: 'createBy' },
-  { title: '更新时间', dataIndex: 'updateTime' },
+  { title: '函数名', dataIndex: 'functionName' },
   { title: '类名', dataIndex: 'className' },
   { title: '模板分类', dataIndex: 'tplCategory' },
-  { title: '包名', dataIndex: 'packageName' },
   { title: '模块名', dataIndex: 'moduleName' },
   { title: '业务名', dataIndex: 'businessName' },
-  { title: '函数名', dataIndex: 'functionName' },
-  { title: '函数作者', dataIndex: 'functionAuthor' },
-  { title: '生成类型', dataIndex: 'genType' },
 ]);
 
 export { codeColumns };
