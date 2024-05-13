0a1674ff96a59871bff0b83394cbd8ec91ad770b
in-git <444891953@qq.com>
2024-05-13 10:44:42 +0800

    打包部署到inscode

--- frontend/src/views/desktop/toolbar/profile-form/Avatar.vue
+++ frontend/src/views/desktop/toolbar/profile-form/Avatar.vue
@@ -14,7 +14,7 @@
       title="选择头像"
       :body-style="{ padding: '0' }"
     >
-      <Gallery type="avatar" @update:value="uploadFile" v-model:value="userProfile.avatar" />
+      <Gallery type="avatar" @update="uploadFile" v-model="userProfile.avatar" />
     </a-modal>
   </div>
 </template>
